ce22ece74d51fe8d1b34e73cdb527444eec76cec
Andreyev Kyrylo <K.Andreyev.FIT.121.20@knute.edu.ua>
2021-11-27 11:23:18 +0200

    Implement PlayerDto class

---
+++ ProxetTournamentFinale/DataTransferObjects/PlayerDto.cs
@@ -0,0 +1,9 @@
+namespace ProxetTournamentFinale.DataTransferObjects
+{
+    public class PlayerDto
+    {
+        public string UserName { get; set; }
+
+        public int VehicleType { get; set; }
+    }
+}
--- ProxetTournamentFinale/Models/Player.cs
+++ ProxetTournamentFinale/Models/Player.cs
@@ -1,5 +1,7 @@
 using System;
 
+using ProxetTournamentFinale.DataTransferObjects;
+
 namespace ProxetTournamentFinale.Models
 {
     public class Player
@@ -11,5 +13,20 @@ namespace ProxetTournamentFinale.Models
         public int VehicleType { get; set; }
 
         public DateTime EnqueueTime { get; set; }
+
+        public Player(PlayerDto dto)
+        {
+            // As far as I know,
+            // the database handles
+            // Id property by itself
+            // so it's pointless
+            // to set it here
+            Id = 0;
+
+            UserName = dto.UserName;
+            VehicleType = dto.VehicleType;
+
+            EnqueueTime = DateTime.UtcNow;
+        }
     }
 }
